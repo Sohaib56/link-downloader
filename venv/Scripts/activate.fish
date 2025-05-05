96ab3cdc2923f266d7b03ff73bdd170f8e297d40
Sohaib ALi <sohaib.ali@seersco.com>
2025-05-05 11:41:46 +0500

    new venv

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\CYBO-ArsalanKhan\yt-downloader-python\venv'
+set -gx VIRTUAL_ENV 'C:\yt-downloader-python\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
